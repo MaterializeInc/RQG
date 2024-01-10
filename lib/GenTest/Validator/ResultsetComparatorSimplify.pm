276bda98f0443473dc1632ae4b10a654e1213f99
Philip Stoev <pstoev@materialize.com>
2024-01-10 14:46:52 +0100

    Fix the ResultsetComparatorSimplify.pm
    - allow simplification of EXPLAIN queries
    - account for differences between Mz and Postgres

--- lib/GenTest/Validator/ResultsetComparatorSimplify.pm
+++ lib/GenTest/Validator/ResultsetComparatorSimplify.pm
@@ -43,12 +43,10 @@ my $query_obj;
 sub validate {
 	my ($comparator, $executors, $results) = @_;
 
-	return STATUS_WONT_HANDLE if $#$results != 1;
+	die "ResultsetComparatorSimplify requires two resultsets" if $#$results != 1;
 
-	return STATUS_WONT_HANDLE if $results->[0]->query() =~ m{EXPLAIN}sio;
-
-	if ( $results->[0]->err() != $results->[1]->err() ) {
-		say("Query: ".$results->[0]->query()."; failed: error code mismatch between servers ('".$results->[0]->errstr()."' vs. '".$results->[1]->errstr()."')");
+	if ( $results->[0]->status() ne $results->[1]->status() ) {
+		say("Query: ".$results->[0]->query()."; failed: error status mismatch between servers ('".$results->[0]->errstr()."' vs. '".$results->[1]->errstr()."')");
 		return STATUS_ERROR_MISMATCH;
 	}
 
@@ -66,21 +64,17 @@ sub validate {
 	) {
 		say("---------- RESULT COMPARISON ISSUE START ----------");
 	}
-		
+
 	if ($compare_outcome == STATUS_LENGTH_MISMATCH) {
-		if ($query =~ m{^\s*select}io) {
-	                say("Query: $query; failed: result length mismatch between servers (".$results->[0]->rows()." vs. ".$results->[1]->rows().")");
-			say(GenTest::Comparator::dumpDiff($results->[0], $results->[1]));
-		} else {
-	                say("Query: $query; failed: affected_rows mismatch between servers (".$results->[0]->affectedRows()." vs. ".$results->[1]->affectedRows().")");
-		}
+		say("Query: $query; failed: result length mismatch between servers (".$results->[0]->rows()." vs. ".$results->[1]->rows().")");
+		say(GenTest::Comparator::dumpDiff($results->[0], $results->[1]));
 	} elsif ($compare_outcome == STATUS_CONTENT_MISMATCH) {
 		say("Query: $query; failed: result content mismatch between servers.");
 		say(GenTest::Comparator::dumpDiff($results->[0], $results->[1]));
 	}
 
 	if (
-		($query =~ m{^\s*select}sio) && (
+		($query =~ m{^\s*select|explain}sio) && (
 			($compare_outcome == STATUS_LENGTH_MISMATCH) ||
 			($compare_outcome == STATUS_CONTENT_MISMATCH)
 		)
@@ -89,48 +83,75 @@ sub validate {
 			oracle => sub {
 				my $oracle_query = shift;
 
-				my @oracle_results;
-				foreach my $executor (@$executors) {
-					my $oracle_result = $executor->execute($oracle_query, 1);
+                return ORACLE_ISSUE_NO_LONGER_REPEATABLE if (($query =~ m{ORDER\s+BY}sgio) && ($oracle_query !~ m{ORDER\s+BY}sgio));
 
-					return ORACLE_ISSUE_STATUS_UNKNOWN if $oracle_result->status() != STATUS_OK;
-					return ORACLE_ISSUE_STATUS_UNKNOWN if defined $oracle_result->warnings();
+				# Prevent the removal of * from SELECT *, as this makes the resulting query unparseable
+				return ORACLE_ISSUE_NO_LONGER_REPEATABLE if (($query =~ m{\*}sgio) && ($oracle_query !~ m{\*}sgio));
 
-					push @oracle_results, $oracle_result;
+				# Prevent the reduction of e.g. COUNT(a1.f1) to COUNT(a1) as this is confusing
+				return ORACLE_ISSUE_NO_LONGER_REPEATABLE if $oracle_query =~ m{(MIN|MAX|AVG|COUNT)\s*\(\s*(t1|t2|t3|a1|a2|a3)\s*\)}sgio;
+
+				foreach my $try (1..1) {
+
+					my @oracle_results;
+					foreach my $executor (@$executors) {
+						my $oracle_result = $executor->execute($oracle_query, 1);
+
+						return ORACLE_ISSUE_STATUS_UNKNOWN if $oracle_result->status() != STATUS_OK;
+						return ORACLE_ISSUE_STATUS_UNKNOWN if defined $oracle_result->warnings();
+
+						# Reject the simplification if the result set contains sets
+
+						if (defined $oracle_result->data()) {
+							foreach my $row (@{$oracle_result->data()}) {
+								foreach my $col (@$row) {
+									return ORACLE_ISSUE_NO_LONGER_REPEATABLE if $col =~ m{^\(}sgio;
+								}
+							}
+						}
+
+						push @oracle_results, $oracle_result;
+					}
+
+
+					my $oracle_compare = GenTest::Comparator::compare($oracle_results[0], $oracle_results[1]);
+
+					#
+					# If both result sets are empty, we can not decide if the issue continues to be repeatable
+					# or not. So, to be safe, we return "unknown", otherwise we risk messing up the differential
+					# coverage reports
+					#
+
+					if (($oracle_results[0]->rows() == 0) && ($oracle_results[1]->rows() == 0)) {
+						return ORACLE_ISSUE_STATUS_UNKNOWN;
+					}
+
+					if (
+						($oracle_compare == STATUS_LENGTH_MISMATCH) ||
+						($oracle_compare == STATUS_CONTENT_MISMATCH)
+					) {
+						return ORACLE_ISSUE_STILL_REPEATABLE;
+					}
 				}
 
-				my $oracle_compare = GenTest::Comparator::compare($oracle_results[0], $oracle_results[1]);
-
-				#
-				# If both result sets are empty, we can not decide if the issue continues to be repeatable
-				# or not. So, to be safe, we return "unknown", otherwise we risk messing up the differential
-				# coverage reports
-				#
-
-				if (($oracle_results[0]->rows() == 0) && ($oracle_results[1]->rows() == 0)) {
-					return ORACLE_ISSUE_STATUS_UNKNOWN;
-				} elsif (
-					($oracle_compare == STATUS_LENGTH_MISMATCH) ||
-					($oracle_compare == STATUS_CONTENT_MISMATCH)
-				) {
-					return ORACLE_ISSUE_STILL_REPEATABLE;
-				} else {
-					return ORACLE_ISSUE_NO_LONGER_REPEATABLE;
-				}
+				return ORACLE_ISSUE_NO_LONGER_REPEATABLE;
 		        }
 		);
 
+        say("Simplifying...");
 		my $simplified_query = $simplifier_sql->simplify($query);
 		
 		if (defined $simplified_query) {
 			say("Simplified query: $simplified_query;");
 
-			my @explains = (
-				$executors->[0]->execute("EXPLAIN ".$simplified_query),
-				$executors->[1]->execute("EXPLAIN ".$simplified_query)
-			);
-	                say("EXPLAIN diff:");
-			say(GenTest::Comparator::dumpDiff(@explains));
+			if ($simplified_query !~ m{EXPLAIN}sgio) {
+				my @explains = (
+					$executors->[0]->execute("EXPLAIN ".$simplified_query),
+					$executors->[1]->execute("EXPLAIN ".$simplified_query)
+				);
+	                	say("EXPLAIN diff:");
+				say(GenTest::Comparator::dumpDiff(@explains));
+			}
 
 			my $simplified_results = [];
 			$simplified_results->[0] = $executors->[0]->execute($simplified_query, 1);
@@ -138,20 +159,22 @@ sub validate {
 			say("Result set diff:");
 			say(GenTest::Comparator::dumpDiff($simplified_results->[0], $simplified_results->[1]));
 
-			my $simplifier_test = GenTest::Simplifier::Test->new(
-				executors	=> $executors,
-				results		=> [ $simplified_results , $results ]
-			);
+
+# Applies to MySQL only, so disabled here
+#			my $simplifier_test = GenTest::Simplifier::Test->new(
+#				executors	=> $executors,
+#				results		=> [ $simplified_results , $results ]
+#			);
 			# show_index is enabled for result difference queries its good to see the index details,
 			# the value 1 is used to define if show_index is enabled, to disable dont assign a value.
-			my $show_index = 1;
-			my $simplified_test = $simplifier_test->simplify($show_index);
+#			my $show_index = 1;
+#			my $simplified_test = $simplifier_test->simplify($show_index);
 
-			my $tmpfile = tmpdir().abs($$).time().".test";
-			say("Dumping .test to $tmpfile");
-			open (TESTFILE, '>'.$tmpfile);
-			print TESTFILE $simplified_test;
-			close TESTFILE;
+#			my $tmpfile = tmpdir().abs($$).time().".test";
+#			say("Dumping .test to $tmpfile");
+#			open (TESTFILE, '>'.$tmpfile);
+#			print TESTFILE $simplified_test;
+#			close TESTFILE;
 		} else {
 			say("Could not simplify failure, appears to be sporadic.");
 		}
@@ -161,6 +184,7 @@ sub validate {
 	     ($compare_outcome == STATUS_CONTENT_MISMATCH) 
 	) {
 		say("---------- RESULT COMPARISON ISSUE END ------------");
+#               die;
 	}
 
 	#
