68f56378a83e8928da68b21efb5db2806bb28e1b
Philip Stoev <pstoev@materialize.com>
2024-01-16 13:24:39 +0100

    fix comment syntax

--- conf/mz/dbt3-ddl-refresh-mvs.sql
+++ conf/mz/dbt3-ddl-refresh-mvs.sql
@@ -1,6 +1,6 @@
-# Refreshing the views is required in Postgres, otherwise they
-# will be empty as they were created before any data was loaded
-# in the base tables.
+-- Refreshing the views is required in Postgres, otherwise they
+-- will be empty as they were created before any data was loaded
+-- in the base tables.
 
 REFRESH MATERIALIZED VIEW customer_pk;
 REFRESH MATERIALIZED VIEW orders_pk;
